8aae5ba2657316fb1af9dfc4c30e3648ae505cb6
kanak kingrani <kanakkingrani81@gmail.com>
2025-05-19 02:37:41 +0530

    Add files via upload

---
+++ Week 0/einstein.py
@@ -0,0 +1,7 @@
+c = 300000000
+#take the value of m from the user.
+m = float(input("Enter the value of m: "))
+#define the formula.
+e = m*c*c
+#print the output.
+print(f"E: {e:,.0f}")
---
+++ Week 0/playback.py
@@ -0,0 +1,7 @@
+msg = input("what are you thinking? ")
+
+#divide/seperate the words by ...
+new_msg = msg.replace(" ", "...")
+
+#print the output on the console
+print(new_msg)
