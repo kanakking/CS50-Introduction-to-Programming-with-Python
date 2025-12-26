2712774dddf61b3764d37ab0c06b110b6f57bfd7
kanak kingrani <kanakkingrani81@gmail.com>
2025-12-26 12:45:48 +0530

    Implement number guessing game

---
+++ Week 4/game.py
@@ -0,0 +1,29 @@
+import random
+
+def main():
+
+    while True:
+        try:
+            n = int(input("Level: "))
+            if n > 0:
+                break
+        except ValueError:
+            pass
+    num = random.randint(1, n)
+
+    while True:
+        try:
+            guess = int(input("Guess: "))
+            if guess > 0:
+                if guess < num:
+                    print("Too small!")
+                elif guess > num:
+                    print("Too large!")
+                else:
+                    print("Just right!")
+                    break
+        except ValueError:
+            pass
+
+if __name__ == "__main__":
+    main()
